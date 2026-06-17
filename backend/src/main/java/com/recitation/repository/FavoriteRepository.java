6308ace5db3816628512172637659319bd70c6c9
QDqiaodong <573717339@qq.com>
2026-06-17 11:50:46 +0800

    其他模型修复编译问题

--- backend/src/main/java/com/recitation/repository/FavoriteRepository.java
+++ backend/src/main/java/com/recitation/repository/FavoriteRepository.java
@@ -4,7 +4,9 @@ import com.recitation.entity.Favorite;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
